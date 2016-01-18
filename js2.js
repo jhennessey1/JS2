e2cc85eb71bfa13ff0e2376ce53ee07d4ad87e82
Student <student@RU-005.local>
2016-01-18 11:32:15 -0700

    complete

--- js2.js
+++ js2.js
@@ -1,3 +1,3 @@
 var anyWord = prompt("Enter any word.");
 
-alert("You entered " + anyWord + ". This word has " + anyWord.length + " characters.")
+alert("You entered " + anyWord + ". This word has " + anyWord.length + " characters. The third character is " + anyWord.charAt(2) + ". Lowercase: " + anyWord.toLowerCase() + ". Uppercase: " + anyWord.toUpperCase() + ". Example: I throw " + anyWord + ". Subword: " + anyWord.substring(1, 4) + ".")
